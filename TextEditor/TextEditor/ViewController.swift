fa588acb6934e1513fd8a97c2372bddea9f93ce2
ChinaHackers <93428739@qq.com>
2019-05-01 19:36:41 +0800

    Create TextEditor Project.

---
+++ TextEditor/TextEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TextEditor
+//
+//  Created by Liu Chuan on 2020/4/7.
+//  Copyright © 2020 Liu Chuan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ TextEditor/TextEditor/WindowController.swift
@@ -0,0 +1,19 @@
+//
+//  WindowController.swift
+//  TextEditor
+//
+//  Created by Liu Chuan on 2020/4/7.
+//  Copyright © 2020 Liu Chuan. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
